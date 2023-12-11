c062d3146c6f3b89a92b3bbed3edc4fd7d18c31b
mihaipaulorban <mihaipaulorban@gmail.com>
2023-12-11 13:24:49 +0100

    feat: sfondo con mixin e inizio griglia comics

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,21 +1,36 @@
 <script>
+import MainProducts from './MainProducts.vue';
 export default {
   name: 'main',
+  components: {
+    MainProducts,
+  },
 };
 </script>
 <template>
   <main>
-    <div class="content">Content Goes Here</div>
+    <div class="main-jumbo"></div>
+    <MainProducts />
   </main>
 </template>
 <style scoped lang="scss">
 @use '../styles/partials/variables.scss' as *;
 @use '../styles/general.scss' as *;
+@use '../styles/partials/mixins.scss' as *;
 
 main {
   background-color: $secondary;
-  color: white;
-  padding: 50px 300px;
-  font-size: 30px;
+
+  .main-jumbo {
+    @include bgImg(
+      '../public/img/jumbotron.jpg',
+      'no-repeat',
+      'center',
+      'cover'
+    );
+    background-size: cover;
+
+    height: 400px;
+  }
 }
 </style>
---
+++ src/components/MainProducts.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: 'MainProducts',
+};
+</script>
+
+<template>
+  <section>
+    <h2>Comic</h2>
+    <div>Copertine</div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables.scss' as *;
+section {
+  height: 500px;
+  padding: 60px;
+  text-align: center;
+}
+</style>
